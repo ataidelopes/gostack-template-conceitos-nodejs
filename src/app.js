0593f501790363bd68b53d139ae33f74c1bc8f2e
Rodrigo Ataide <02997774136>
2020-08-18 19:15:46 -0300

    finalizing the concepts-nodejs challenge

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
@@ -11,23 +10,73 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs} = request.body;
+
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  }
+
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const { title, url, techs } = request.body;
+
+  const index = findIndexById(id);
+
+  if( index < 0 ){
+    return response.status(400).send();
+  }
+
+  const repository = { ...repositories[index], title, url, techs }
+  
+  repositories[index] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const index = findIndexById(id);
+
+  if( index < 0 ) {
+    return response.status(400).send();
+  }
+
+  repositories.splice(index, 1);
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const repository = findById(id);
+
+  if(!repository) {
+    return response.status(400).send();
+  }
+
+  repository.likes += 1;
+
+  return response.json(repository);
+
 });
+
+const findById = (id) => repositories.find(repositorie => repositorie.id === id);
+const findIndexById = (id) => repositories.findIndex(repositorie => repositorie.id === id);
 
 module.exports = app;
